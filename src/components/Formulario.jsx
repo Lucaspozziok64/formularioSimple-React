8caf05196b3fdff67c2f16126e98fe9109566873
Lucasfi <lukafigueroa64@gmail.com>
2025-06-22 21:03:20 -0300

    logramos capturar los datos del input del form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,26 +1,52 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import "../App.css"
+import "../App.css";
+import { useState } from "react";
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [dni, setDni] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <section className="container bg-secondary rounded-3">
       <Form>
         <Form.Group className="mb-3" controlId="formBasicNombre">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Nombre" required />
+          <Form.Control
+            type="text"
+            placeholder="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicApellido">
           <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Apellido" required />
+          <Form.Control
+            type="text"
+            placeholder="Apellido"
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicDni">
           <Form.Label>Dni</Form.Label>
-          <Form.Control type="text" placeholder="Dni" required />
+          <Form.Control
+            type="text"
+            placeholder="Dni"
+            value={dni}
+            onChange={(e) => setDni(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="ejemplo@gmail.com" required />
+          <Form.Control
+            type="email"
+            placeholder="ejemplo@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Text className="text-muted">
             Nunca compartiremos su correo electrónico con nadie más.
           </Form.Text>
